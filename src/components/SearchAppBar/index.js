b5ee434e1c886e9d063f2ff52631b604f686b3dc
Náder W. S. Sírio <nadersirio@gmail.com>
2023-08-25 15:16:41 -0300

    add: fetch update: app, button files refactor: components folder

---
+++ src/components/SearchAppBar/index.js
@@ -0,0 +1,49 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import SearchIcon from '@mui/icons-material/Search';
+import { CustomizedButtons } from "../../Button";
+import '../../App.css';
+
+import * as S from './styles';
+
+function SearchAppBar({
+  inputFocused,
+  setInputFocused,
+  valueInput,
+  setInput,
+  msg,
+  setMsg,
+  disabled,
+  setDisabled,
+}) {
+  return (
+    <S.Box isFocused={!inputFocused}>
+      <AppBar className='search-app-bar__app-bar' position='static'>
+        <Toolbar>
+          <S.Search>
+            <S.SearchIconWrapper>
+              <SearchIcon />
+            </S.SearchIconWrapper>
+            <S.StyledInputBase
+              placeholder="Insira sua cidade"
+              value={valueInput}
+              onChange={event => setInput(event.target.value)}
+              inputProps={{ 'aria-label': 'search' }}
+              onFocus={() => setInputFocused(true)}
+              onBlur={() => setInputFocused(false)}
+              disabled={disabled}
+            />
+          </S.Search>
+          <CustomizedButtons
+            valueInput={valueInput}
+            msg={msg}
+            setMsg={setMsg}
+            setHideModal={setDisabled}
+          />
+        </Toolbar>
+      </AppBar>
+    </S.Box>
+  );
+}
+
+export default SearchAppBar
