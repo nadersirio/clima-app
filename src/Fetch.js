e19d949c175246b43771a2aed986ab26c66c94be
Náder W. S. Sírio <nadersirio@gmail.com>
2023-08-28 20:06:47 -0300

    refactoring: api and key api refactored

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,8 +1,9 @@
-import { api } from './@api-key';
+import Cookies from 'js-cookie';
 
 export const fetchApi = async (query) => {
+  const api_Base = "https://api.openweathermap.org/data/2.5/";
   try {
-    const data = await fetch(`${api.base}weather?q=${query}&lang=pt_br&units=metric&APPID=${api.key}`);
+    const data = await fetch(`${api_Base}weather?q=${query}&lang=pt_br&units=metric&APPID=${Cookies.get('key')}`);
     const clima = await data.json();
     if(data.status !== 200) {
       throw new Error(`${clima.cod}, ${clima.message}`);
